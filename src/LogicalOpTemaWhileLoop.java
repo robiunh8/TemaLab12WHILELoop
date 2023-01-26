8127a19b36bd145d0418b6817cb462c186b99de3
Robert Kerekes <robert.kerekes@yahoo.com>
2023-01-26 23:56:45 +0200

    tema 12 while loop

--- src/LogicalOpTemaWhileLoop.java
+++ src/LogicalOpTemaWhileLoop.java
@@ -1,2 +1,105 @@
-package PACKAGE_NAME;public class LogicalOpTemaWhileLoop {
+public class LogicalOpTemaWhileLoop {
+
+//ex 1
+     public void countTo100(int number) {
+        while (number <= 100) {
+            System.out.print(number+" ");
+            number++;
+        }
+    }
+//ex 2
+public void counttoMinus100(int number) {
+    while (number >= -100) {
+        System.out.print(number+" ");
+        number--;
+    }
+}
+//ex 3
+public void countfirstandsec(int first, int second) {
+    while (first <= second) {
+        System.out.print(first+" ");
+        first++;
+    }
+}
+//ex 4
+public void countDescrEx4(int first, int second) {
+    if(first < second){
+        while (first <= second) {
+            System.out.print(second+" ");
+            second--;
+        }
+    }
+    else if(second < first){
+        while (second <= first) {
+            System.out.print(first+" ");
+            first++;
+        }
+    }
+}
+//ex 5
+public void printevenNumbers() {
+    int a = 1;
+    while (a <= 100) {
+        a++;
+        if (a % 2 == 0) {
+            System.out.print(a+" ");
+        }
+    }
+}
+//ex 6
+public void printOddNumbers() {
+    int a = 1;
+    while (a <= 100) {
+        a++;
+        if (a % 2 != 0) {
+            System.out.print(a+" ");
+        }
+    }
+}
+//ex 7
+public float sumAverageCountWHILEex7(int first, int last){
+    float sum= 0;
+    int count= 0;
+        while(first<=last){
+            sum = sum + first;
+            count++;
+            first++;
+        }
+        return sum/count;
+    }
+//ex 8
+public float divideby7(float first, float second) {
+    float count = 0;
+    float sum = 0;
+    while (first <= second) {
+        if (first % 7 == 0) {
+            count ++;
+            sum += first;
+        }
+        first++;
+    }
+    float average = sum / count;
+    return average;
+}
+//ex 9
+public void printnrFibonacci() {
+    int count = 20;
+    int numOne = 0;
+    int numTwo = 1;
+    int i=1;
+    while(i<=count)
+    {
+        System.out.print(numOne+" ");
+        int sumnum1and2 = numOne + numTwo;
+        numOne = numTwo;
+        numTwo = sumnum1and2;
+        i++;
+    }
+}
+//ex 10
+
+//nu am stiut sa-l fac
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,75 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+LogicalOpTemaWhileLoop TemaWhileLoopEX= new LogicalOpTemaWhileLoop();
+//ex 1
+
+        System.out.println("EX 1");
+TemaWhileLoopEX.countTo100(90);
+//ex 2
+        System.out.println(" ");
+        System.out.println("EX 2");
+        TemaWhileLoopEX.counttoMinus100(-90);
+//ex 3
+        System.out.println(" ");
+        System.out.println("EX 3");
+TemaWhileLoopEX.countfirstandsec(10,20);
+
+//ex 4
+        System.out.println(" ");
+        System.out.println("EX 4");
+TemaWhileLoopEX.countDescrEx4(10,20);
+
+//ex 5
+        System.out.println(" ");
+        System.out.println("EX 5");
+TemaWhileLoopEX.printevenNumbers();
+
+//ex 6
+        System.out.println(" ");
+        System.out.println("EX 6");
+TemaWhileLoopEX.printOddNumbers();
+
+//ex 7
+        System.out.println(" ");
+        System.out.println("EX 7");
+            System.out.println(TemaWhileLoopEX.sumAverageCountWHILEex7(111,8899));
+
+//ex 8
+        System.out.println(" ");
+        System.out.println("EX 8");
+            System.out.println(TemaWhileLoopEX.divideby7(14,49));
+
+//ex 9
+        System.out.println(" ");
+        System.out.println("EX 9");
+TemaWhileLoopEX.printnrFibonacci();
+//ex 10
+        System.out.println(" ");
+        System.out.println("EX 10");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
